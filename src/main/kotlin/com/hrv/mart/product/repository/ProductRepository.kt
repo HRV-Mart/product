8f2477c652956c2e50d5b2cbe66883c9d81c5768
Harsh Verma <55652117+Harsh3305@users.noreply.github.com>
2023-05-22 17:02:46 +0530

    Add product url in application property (#13)
    * Added product.url in application.properties
    
    * Added get cost function
    
    * Added env

--- src/main/kotlin/com/hrv/mart/product/repository/ProductRepository.kt
+++ src/main/kotlin/com/hrv/mart/product/repository/ProductRepository.kt
@@ -3,17 +3,22 @@ package com.hrv.mart.product.repository
 import com.hrv.mart.apicall.APICaller
 import com.hrv.mart.product.Product
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Repository
 import org.springframework.web.reactive.function.client.WebClient
 
 @Repository
 class ProductRepository (
     @Autowired
-    private val webClientBuilder: WebClient.Builder
+    private val webClientBuilder: WebClient.Builder,
+    @Value("\${product.url}")
+    private val productURL: String
 )
 {
-    private val productURL = "http://localhost:8081/product"
     fun getProductByProductId(productId: String) =
         APICaller(webClientBuilder)
             .getData("${productURL}/${productId}", Product::class.java)
+    fun getCostOfProductByProductId(productId: String) =
+        getProductByProductId(productId)
+            .map { it.price }
 }
