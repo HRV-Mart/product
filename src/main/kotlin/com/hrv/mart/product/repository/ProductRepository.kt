25c69ab23ef9690a4a4c4f2eccc691578c7f6ba3
Harsh3305 <harshverma3305@gmail.com>
2023-06-29 23:34:56 +0530

    Remove query params model

--- src/main/kotlin/com/hrv/mart/product/repository/ProductRepository.kt
+++ src/main/kotlin/com/hrv/mart/product/repository/ProductRepository.kt
@@ -3,7 +3,6 @@ package com.hrv.mart.product.repository
 import com.hrv.mart.apicall.APICaller
 import com.hrv.mart.custompageable.Pageable
 import com.hrv.mart.product.model.Product
-import com.hrv.mart.product.model.QueryParams
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.server.reactive.ServerHttpResponse
